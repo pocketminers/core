6647dcd7f6e8c914f3a8ba3854735eb08378059b
boyroywax <jmsdevln@gmail.com>
2025-04-02 20:51:12 -0700

    core:docker added working dockerfile and entrypoint.sh to build core in docker container

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,18 @@
 FROM node:23.9.0-alpine3.21
 
-VOLUME ["/usr/share/build"]
+WORKDIR /usr/app/core/
 
-WORKDIR /app
+COPY entrypoint.sh /usr/app/core/
+RUN chmod +x entrypoint.sh
 
+# Check .dockerignore to ensure that the entrypoint.sh is not ignored
+COPY ./package.json yarn.lock /usr/app/core/
+
+RUN yarn install
+
+COPY tsconfig.json tsconfig.*.json /usr/app/core/
+
+COPY tests/ /usr/app/core/tests/
+COPY src/ /usr/app/core/src/
+
+ENTRYPOINT [ "entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# This script is used to build the core package for the project.
+# It sets up the environment, installs dependencies, and runs the build process.
+
+# Check that the script is being run in the correct directory
+if [ ! -d "/usr/app/core" ]; then
+  echo "This script must be run from the root of the project."
+  exit 1
+fi
+
+# Check that the source directory exists
+if [ ! -d "/usr/app/core/src" ]; then
+  echo "Source directory does not exist."
+  exit 1
+fi
+
+# Check that the node_modules directory exists
+if [ ! -d "/usr/app/core/node_modules" ]; then
+  echo "node_modules directory does not exist. Installing dependencies..."
+  yarn install
+fi
+
+# Lint the code
+echo "Linting the code..."
+yarn lint
+if [ $? -ne 0 ]; then
+  echo "Linting failed. Please fix the errors and try again."
+  exit 1
+fi
+
+# Run the tests
+echo "Running tests..."
+yarn test
+if [ $? -ne 0 ]; then
+  echo "Tests failed. Please fix the errors and try again."
+  exit 1
+fi
+
+# Ensure the script is running in a Production environment
+if [ "$NODE_ENV" != "production" ]; then
+  echo "NODE_ENV is not set to production. Setting it to production..."
+  export NODE_ENV=production
+fi
+
+
+# Build the project
+echo "Building the project..."
+yarn build
+
+if [ $? -ne 0 ]; then
+  echo "Build failed. Please fix the errors and try again."
+  exit 1
+fi
+
+# Check that the build directory exists
+if [ ! -d "/usr/app/core/dist" ]; then
+  echo "Distribute directory does not exist. Build failed."
+  exit 1
+fi
+
+# Check that the build was successful
+if [ -z "$(ls -A /usr/app/core/dist)" ]; then
+  echo "Build directory is empty. Build failed."
+  exit 1
+fi
+
+echo "Build completed successfully."
